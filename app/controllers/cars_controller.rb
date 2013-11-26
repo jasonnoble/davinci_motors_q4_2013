2fa9e3b22e8ab23600447b734c81768faabaaade
Jason Noble <github+jasonn@jasonnoble.org>
2013-11-25 21:37:40 -0700

    Added will_paginate

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,7 +4,7 @@ class CarsController < ApplicationController
   # GET /cars
   # GET /cars.json
   def index
-    @cars = Car.all
+    @cars = Car.paginate(page: params[:page])
   end
 
   # GET /cars/1
